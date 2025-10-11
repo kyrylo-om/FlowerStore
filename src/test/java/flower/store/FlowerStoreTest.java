9695b2da662a78137712471d72eb4c02f069686d
Kyrylo Omelianchuk <kyrylo.omelianchuk@gmail.com>
2025-10-11 17:32:44 +0300

    Fix lint errors again again

--- src/test/java/flower/store/FlowerStoreTest.java
+++ src/test/java/flower/store/FlowerStoreTest.java
@@ -2,26 +2,31 @@ package flower.store;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Random;
 
 import org.junit.jupiter.api.Assertions;
 
 public class FlowerStoreTest {
+    private static final Random RANDOM_GENERATOR = new Random();
+    private static final int MAX_QUANTITY = 1000;
 
     @Test
     public void testStore() {
-        FlowerBucket flowerBucket1 = new FlowerBucket();
-        FlowerBucket flowerBucket2 = new FlowerBucket();
-        Flower flower1 = new Rose();
-        Flower flower2 = new Chamomile();
-        flower1.setPrice(1);
-        FlowerPack flowerPack1 = new FlowerPack(flower1, 2);
-        flowerBucket1.add(flowerPack1);
-        FlowerPack flowerPack2 = new FlowerPack(flower2, 10);
-        flowerBucket2.add(flowerPack2);
+        FlowerBucket firstFlowerBucket = new FlowerBucket();
+        FlowerBucket secondFlowerBucket = new FlowerBucket();
+        Flower firstFlower = new Rose();
+        Flower secondFlower = new Chamomile();
+        FlowerPack firstFlowerPack = new FlowerPack(
+            firstFlower, RANDOM_GENERATOR.nextInt(MAX_QUANTITY));
+        firstFlowerBucket.add(firstFlowerPack);
+        FlowerPack secondFlowerPack = new FlowerPack(
+            secondFlower, RANDOM_GENERATOR.nextInt(MAX_QUANTITY));
+        secondFlowerBucket.add(secondFlowerPack);
 
-        Store store = new Store(Arrays.asList(flowerBucket1, flowerBucket2, flowerBucket1));
-        Assertions.assertEquals(store.search(flowerBucket1), Arrays.asList(flowerBucket1, flowerBucket1));
+        Store store = new Store(Arrays.asList(
+            firstFlowerBucket, secondFlowerBucket, firstFlowerBucket));
+        Assertions.assertEquals(store.search(firstFlowerBucket), 
+        Arrays.asList(firstFlowerBucket, firstFlowerBucket));
     }
 }
